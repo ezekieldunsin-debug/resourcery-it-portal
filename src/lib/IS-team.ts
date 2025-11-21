b4dd9615fb764f2982fae392b2c158d0a96800cf
ezekieldunsin-debug <ezekieldunsin@gmail.com>
2025-11-21 16:26:11 +0100

    Delete src directory

--- src/lib/IS-team.ts
+++
@@ -1,8 +0,0 @@
-export const IS_TEAM_EMAILS = [
-  "ezekiela@resourcery.com",
-  "malikm@resourcery.com",
-  "is@resourcery.com",
-  // ← add all IS emails
-].map(e => e.toLowerCase());
-
-export const isISUser = (email: string) => IS_TEAM_EMAILS.includes(email.toLowerCase());
