e7855ccaaef0cebf17896c5e9c4a67a94abc2853
ezekieldunsin-debug <ezekieldunsin@gmail.com>
2025-11-21 14:21:44 +0100

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        navy: '#0F172A',
+        teal: '#00D4AA',
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
